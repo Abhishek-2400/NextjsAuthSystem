e637027d7ce982439726e34bffe030bac65c23ae
Abhishek-2400 <abhi2003shukla@gmail.com>
2024-06-06 11:38:38 +0530

    login / signup frontend functionality added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import Link from 'next/link'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 const page = () => {
+
+    const [payload, setpayload] = useState({
+        "email": "",
+        "password": ""
+    })
+
+    const OnChangeHandler = (e) => {
+        console.log(payload)
+        setpayload({ ...payload, [e.target.name]: e.target.value })
+    }
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        console.log(payload);
+    }
+
     return (
-        <div>
-            Hello inside login
-        </div>
+        <form onSubmit={handlesubmit}>
+            <h1>Login</h1>
+            <div className="email">
+                <label htmlFor='email'>Email:</label>
+                <input type="email" id="email" name="email" required value={payload.email} onChange={(e) => { OnChangeHandler(e) }} />
+            </div>
+            <div className="password">
+                <label htmlFor='password'>Password:</label>
+                <input type="password" id="password" name="password" required value={payload.password} onChange={(e) => { OnChangeHandler(e) }} />
+            </div>
+            <div className="submit">
+                <button type="submit">Login</button>
+            </div>
+            <Link href='/signup'>Signup</Link>
+        </form>
     )
 }
 
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-
+"use client"
+import React, { useState } from 'react'
+import Link from 'next/link'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 const signup = () => {
+
+    const [payload, setpayload] = useState({
+        "email": "",
+        "username": "",
+        "password": ""
+    })
+
+    const OnChangeHandler = (e) => {
+        console.log(payload)
+        setpayload({ ...payload, [e.target.name]: e.target.value })
+    }
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        console.log(payload);
+    }
     return (
-        <div>
-            Signup
-        </div>
+
+        <form onSubmit={handlesubmit}>
+            <h1>Signup</h1>
+            <div className="username">
+                <label htmlFor='username'>Username:</label>
+                <input type="text" id="username" name="username" required value={payload.username} onChange={(e) => { OnChangeHandler(e) }} />
+            </div>
+            <div className="email">
+                <label htmlFor='email'>Email:</label>
+                <input type="email" id="email" name="email" required value={payload.email} onChange={(e) => { OnChangeHandler(e) }} />
+            </div>
+            <div className="password">
+                <label htmlFor='password'>Password:</label>
+                <input type="password" id="password" name="password" required value={payload.password} onChange={(e) => { OnChangeHandler(e) }} />
+            </div>
+            <div className="submit">
+                <button type="submit">Signup</button>
+            </div>
+            <Link href='/login'>Login</Link>
+        </form>
+
     )
 }
 
